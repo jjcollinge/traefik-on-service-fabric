e74871326996c04b0c60b30a427b40cdd34ab73b
jjcollinge <jonathancollinge@live.com>
2017-09-07 09:01:38 +0100

    Update existing solution before copying out app package

--- Traefik/Scripts/Update-Config.ps1
+++ Traefik/Scripts/Update-Config.ps1
@@ -1,22 +1,28 @@
-﻿# In order to update the configuration
-# for an existing service you need to
-# create a package that only contains
-# the following files:
-#
-# ApplicationManifest.xml
-# ServiceManifest.xml
-# Config/...
-#
-# Perform the following updates on these files
-#
-# ApplicationManifest.xml
-#	- ApplicationTypeVersion
-#	- ServiceManifestImport -> ServiceManifestRef -> SeviceManifestVersion 
-# ServiceManifest.xml
-#	- Version
-#	- ConfigPackage -> Version
+﻿# The following script is meant to perform a config
+# only in-place upgrade. This allows you to update
+# just config files/values and deploy them against
+# an existing code and data package.
 
-# Setup paths
+# WARNING
+# -----------------------------
+# This script expects the fabric:/Traefik application
+# to already be deployed. It also expects the current
+# solution versions to match the deployed versions of
+# the code/config/data packages.
+
+ param (
+    [Parameter(Mandatory=$false)]
+    [string]
+    $ServiceFabricConnectionEndpoint = "localhost:19000",
+    [Parameter(Mandatory=$false)]
+    [string]
+    $ServiceFabricImageStore = "file:C:\SfDevCluster\Data\ImageStoreShare"
+ )
+
+#
+# Setup required paths to manifest files and directories
+#
+
 $ApplicationPackageRoot = [io.Path]::Combine((Split-Path -Path $pwd.Path -Parent), "ApplicationPackageRoot")
 $TraefikPackage = [io.Path]::Combine($ApplicationPackageRoot, "TraefikPkg")
 $ConfigPackages = [io.Path]::Combine($ApplicationPackageRoot, "ConfigPkgs")
@@ -27,62 +33,74 @@ if ( -Not (Test-Path -LiteralPath $ConfigPackages -PathType Container))
 	mkdir $ConfigPackages
 }
 
-# Read in existing configuration
+#
+# Update the existing config files to reflect a patched configuration
+#
+
+# Read current service manifest
 $CurrentServiceManifestPath = [io.Path]::Combine($TraefikPackage, "ServiceManifest.xml")
 [xml]$CurrentServiceManifestXML = Get-Content -Path $CurrentServiceManifestPath
-$CurrentConfigVersion = $CurrentServiceManifestXML.ServiceManifest.ConfigPackage.Version
 
-# Create new config package
-$Major, $Minor, $Patch = $CurrentConfigVersion.Split('.')
-$IncrementedPatch = [int]$Patch + 1
-$NewConfigVersion = $Major + "." + $Minor + "." + $IncrementedPatch
-$NewPackageName = "ConfigPkg" + $NewConfigVersion
-$NewPackage = [io.Path]::Combine($ConfigPackages, $NewPackageName)
-mkdir $NewPackage
-$NewServicePackage = [io.Path]::Combine($NewPackage, "TraefikPkg")
-mkdir $NewServicePackage
+# Increment service manifest config package patch version
+$SerConfPkgVerMajor, $SerConfPkgVerMinor, $SerConfPkgVerPatch = $CurrentServiceManifestXML.ServiceManifest.ConfigPackage.Version.Split('.')
+$SerConfPkgVerPatch = [int]$SerConfPkgVerPatch + 1
+$NewSerConfPkgVersion = $SerConfPkgVerMajor + "." + $SerConfPkgVerMinor + "." + $SerConfPkgVerPatch
+$CurrentServiceManifestXML.ServiceManifest.ConfigPackage.Version = $NewSerConfPkgVersion
 
-# Copy existing config into package
-Copy-Item $CurrentServiceManifestPath $NewServicePackage
-$CurrentApplicationManifest = [io.Path]::Combine($ApplicationPackageRoot, "ApplicationManifest.xml")
-Copy-Item $CurrentApplicationManifest $NewPackage
+# Increment service manifest patch version
+$SerVerMajor, $SerVerMinor, $SerVerPatch = $CurrentServiceManifestXML.ServiceManifest.Version.Split('.')
+$SerVerPatch = [int]$SerVerPatch + 1
+$NewSerVersion = $SerVerMajor + "." + $SerVerMinor + "." + $SerVerPatch
+$CurrentServiceManifestXML.ServiceManifest.Version = $NewSerVersion
 
-# Copy new config folder into package
+# Write updates to service manifest
+$CurrentServiceManifestXML.Save($CurrentServiceManifestPath)
+
+# Read current application manifest
+$CurrentApplicationManifestPath = [io.Path]::Combine($ApplicationPackageRoot, "ApplicationManifest.xml")
+[xml]$CurrentApplicationManifestXML = Get-Content -Path $CurrentApplicationManifestPath
+
+# Update application manifest service manifest import version
+$CurrentApplicationManifestXML.ApplicationManifest.ServiceManifestImport.FirstChild.ServiceManifestVersion = $NewSerVersion
+
+# Increment application manifest application type version
+$AppTypeVerMajor, $AppTypeVerMinor, $AppTypeVerPatch = $CurrentApplicationManifestXML.ApplicationManifest.ApplicationTypeVersion.Split('.')
+$AppTypeVerPatch = [int]$AppTypeVerPatch + 1
+$NewAppTypeVer = $AppTypeVerMajor + "." + $AppTypeVerMinor + "." + $AppTypeVerPatch
+$CurrentApplicationManifestXML.ApplicationManifest.ApplicationTypeVersion = $NewAppTypeVer
+
+# Write updates to application manifest
+$CurrentApplicationManifestXML.Save($CurrentApplicationManifestPath)
+
+#
+# Create a new (config only) application package to deploy to the Service Fabric cluster
+#
+
+# Create new application package
+$NewAppPkgName = "ConfigPkg" + $NewAppTypeVer
+$NewAppPkg = [io.Path]::Combine($ConfigPackages, $NewAppPkgName)
+mkdir $NewAppPkg
+
+# Create new service package
+$NewSerPkg = [io.Path]::Combine($NewAppPkg, "TraefikPkg")
+mkdir $NewSerPkg
+
+# Copy existing service manifest to new service package
+Copy-Item $CurrentServiceManifestPath $NewSerPkg
+
+# Copy updated config package to new service package
 $CurrentConfig = [io.Path]::Combine($TraefikPackage, "Config")
-Copy-Item $CurrentConfig -Recurse -Container -Destination $NewServicePackage
+Copy-Item $CurrentConfig -Recurse -Container -Destination $NewSerPkg
 
-# Read service manifest
-$NewServiceManifestPath = [io.Path]::Combine($NewServicePackage, "ServiceManifest.xml")
-[xml]$NewServiceManifestXML = Get-Content -Path $NewServiceManifestPath
+# Copy existing application manifest to new application package
+Copy-Item $CurrentApplicationManifestPath $NewAppPkg
 
-# Update service manifest version
-$SerVerMajor, $SerVerMinor, $SerVerPatch = $NewServiceManifestXML.ServiceManifest.Version.Split('.')
-$IncremenetedSerVerPatch = [int]$SerVerPatch + 1
-$NewSerVer = $SerVerMajor + "." + $SerVerMinor + "." + $IncremenetedSerVerPatch
-$NewServiceManifestXML.ServiceManifest.Version = $NewSerVer
+#
+# Deploy new application package to Service Fabric cluster
+#
 
-# Update service manifest code package version
-$NewServiceManifestXML.ServiceManifest.ConfigPackage.Version = $NewSerVer
-$NewServiceManifestXML.Save($NewServiceManifestPath)
-
-# Read application manifest
-$NewApplicationManifestPath = [io.Path]::Combine($NewPackage, "ApplicationManifest.xml")
-[xml]$NewApplicationManifestXML = Get-Content -Path $NewApplicationManifestPath
-
-# Update application manifest type version
-$AppVerMajor, $AppVerMinor, $AppVerPatch = $NewApplicationManifestXML.ApplicationManifest.ApplicationTypeVersion.Split('.')
-$IncremenetedAppVerPatch = [int]$AppVerPatch + 1
-$NewAppVer = $AppVerMajor + "." + $AppVerMinor + "." + $IncremenetedAppVerPatch
-$NewApplicationManifestXML.ApplicationManifest.ApplicationTypeVersion = $NewAppVer
-
-# Update appliction manifest service manifest import version
-$NewApplicationManifestXML.ApplicationManifest.ServiceManifestImport.FirstChild.ServiceManifestVersion = $NewSerVer
-$NewApplicationManifestXML.Save($NewApplicationManifestPath)
-
-# Deploy package to Service Fabric Cluster
-$ServiceFabricImageStore = "file:C:\SfDevCluster\Data\ImageStoreShare"
 $TraefikApplicationName = "fabric:/Traefik"
-Connect-ServiceFabricCluster -ConnectionEndpoint localhost:19000
-Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $NewPackage -ImageStoreConnectionString $ServiceFabricImageStore -ApplicationPackagePathInImageStore $NewPackageName
-Register-ServiceFabricApplicationType -ApplicationPathInImageStore $NewPackageName
-Start-ServiceFabricApplicationUpgrade -ApplicationName $TraefikApplicationName -ApplicationTypeVersion $NewConfigVersion -HealthCheckStableDurationSec 60 -UpgradeDomainTimeoutSec 1200 -UpgradeTimeout 3000 -FailureAction Rollback -Monitored
+Connect-ServiceFabricCluster -ConnectionEndpoint $ServiceFabricConnectionEndpoint
+Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $NewAppPkg -ImageStoreConnectionString $ServiceFabricImageStore -ApplicationPackagePathInImageStore $NewAppPkgName
+Register-ServiceFabricApplicationType -ApplicationPathInImageStore $NewAppPkgName
+Start-ServiceFabricApplicationUpgrade -ApplicationName $TraefikApplicationName -ApplicationTypeVersion $NewAppTypeVer -HealthCheckStableDurationSec 60 -UpgradeDomainTimeoutSec 1200 -UpgradeTimeout 3000 -FailureAction Rollback -Monitored
