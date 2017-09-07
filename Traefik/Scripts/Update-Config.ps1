7416d74ed31b9a3079a01b4418d0a83933eb46d3
jjcollinge <jonathancollinge@live.com>
2017-09-07 16:02:08 +0100

    Moved static config into code file

--- Traefik/Scripts/Update-Config.ps1
+++ Traefik/Scripts/Update-Config.ps1
@@ -2,13 +2,15 @@
 # only in-place upgrade. This allows you to update
 # just config files/values and deploy them against
 # an existing code and data package.
-
-# WARNING
-# -----------------------------
+#
+#						WARNING
+# ---------------------------------------------------
 # This script expects the fabric:/Traefik application
 # to already be deployed. It also expects the current
 # solution versions to match the deployed versions of
 # the code/config/data packages.
+# ---------------------------------------------------
+#
 
  param (
     [Parameter(Mandatory=$false)]
@@ -39,68 +41,86 @@ if ( -Not (Test-Path -LiteralPath $ConfigPackages -PathType Container))
 
 # Read current service manifest
 $CurrentServiceManifestPath = [io.Path]::Combine($TraefikPackage, "ServiceManifest.xml")
+$CurrentServiceManifestBackupPath = $CurrentServiceManifestPath + ".bak"
+Copy-Item $CurrentServiceManifestPath $CurrentServiceManifestBackupPath
 [xml]$CurrentServiceManifestXML = Get-Content -Path $CurrentServiceManifestPath
-
-# Increment service manifest config package patch version
-$SerConfPkgVerMajor, $SerConfPkgVerMinor, $SerConfPkgVerPatch = $CurrentServiceManifestXML.ServiceManifest.ConfigPackage.Version.Split('.')
-$SerConfPkgVerPatch = [int]$SerConfPkgVerPatch + 1
-$NewSerConfPkgVersion = $SerConfPkgVerMajor + "." + $SerConfPkgVerMinor + "." + $SerConfPkgVerPatch
-$CurrentServiceManifestXML.ServiceManifest.ConfigPackage.Version = $NewSerConfPkgVersion
-
-# Increment service manifest patch version
-$SerVerMajor, $SerVerMinor, $SerVerPatch = $CurrentServiceManifestXML.ServiceManifest.Version.Split('.')
-$SerVerPatch = [int]$SerVerPatch + 1
-$NewSerVersion = $SerVerMajor + "." + $SerVerMinor + "." + $SerVerPatch
-$CurrentServiceManifestXML.ServiceManifest.Version = $NewSerVersion
-
-# Write updates to service manifest
-$CurrentServiceManifestXML.Save($CurrentServiceManifestPath)
 
 # Read current application manifest
 $CurrentApplicationManifestPath = [io.Path]::Combine($ApplicationPackageRoot, "ApplicationManifest.xml")
+$CurrentApplicationManifestBackupPath = $CurrentApplicationManifestPath + ".bak"
+Copy-Item $CurrentApplicationManifestPath $CurrentApplicationManifestBackupPath
 [xml]$CurrentApplicationManifestXML = Get-Content -Path $CurrentApplicationManifestPath
 
-# Update application manifest service manifest import version
-$CurrentApplicationManifestXML.ApplicationManifest.ServiceManifestImport.FirstChild.ServiceManifestVersion = $NewSerVersion
+Try {
+	# Increment service manifest config package patch version
+	$SerConfPkgVerMajor, $SerConfPkgVerMinor, $SerConfPkgVerPatch = $CurrentServiceManifestXML.ServiceManifest.ConfigPackage.Version.Split('.')
+	$SerConfPkgVerPatch = [int]$SerConfPkgVerPatch + 1
+	$NewSerConfPkgVersion = $SerConfPkgVerMajor + "." + $SerConfPkgVerMinor + "." + $SerConfPkgVerPatch
+	$CurrentServiceManifestXML.ServiceManifest.ConfigPackage.Version = $NewSerConfPkgVersion
 
-# Increment application manifest application type version
-$AppTypeVerMajor, $AppTypeVerMinor, $AppTypeVerPatch = $CurrentApplicationManifestXML.ApplicationManifest.ApplicationTypeVersion.Split('.')
-$AppTypeVerPatch = [int]$AppTypeVerPatch + 1
-$NewAppTypeVer = $AppTypeVerMajor + "." + $AppTypeVerMinor + "." + $AppTypeVerPatch
-$CurrentApplicationManifestXML.ApplicationManifest.ApplicationTypeVersion = $NewAppTypeVer
+	# Increment service manifest patch version
+	$SerVerMajor, $SerVerMinor, $SerVerPatch = $CurrentServiceManifestXML.ServiceManifest.Version.Split('.')
+	$SerVerPatch = [int]$SerVerPatch + 1
+	$NewSerVersion = $SerVerMajor + "." + $SerVerMinor + "." + $SerVerPatch
+	$CurrentServiceManifestXML.ServiceManifest.Version = $NewSerVersion
 
-# Write updates to application manifest
-$CurrentApplicationManifestXML.Save($CurrentApplicationManifestPath)
+	# Write updates to service manifest
+	$CurrentServiceManifestXML.Save($CurrentServiceManifestPath)
 
-#
-# Create a new (config only) application package to deploy to the Service Fabric cluster
-#
+	# Update application manifest service manifest import version
+	$CurrentApplicationManifestXML.ApplicationManifest.ServiceManifestImport.FirstChild.ServiceManifestVersion = $NewSerVersion
 
-# Create new application package
-$NewAppPkgName = "ConfigPkg" + $NewAppTypeVer
-$NewAppPkg = [io.Path]::Combine($ConfigPackages, $NewAppPkgName)
-mkdir $NewAppPkg
+	# Increment application manifest application type version
+	$AppTypeVerMajor, $AppTypeVerMinor, $AppTypeVerPatch = $CurrentApplicationManifestXML.ApplicationManifest.ApplicationTypeVersion.Split('.')
+	$AppTypeVerPatch = [int]$AppTypeVerPatch + 1
+	$NewAppTypeVer = $AppTypeVerMajor + "." + $AppTypeVerMinor + "." + $AppTypeVerPatch
+	$CurrentApplicationManifestXML.ApplicationManifest.ApplicationTypeVersion = $NewAppTypeVer
 
-# Create new service package
-$NewSerPkg = [io.Path]::Combine($NewAppPkg, "TraefikPkg")
-mkdir $NewSerPkg
+	# Write updates to application manifest
+	$CurrentApplicationManifestXML.Save($CurrentApplicationManifestPath)
 
-# Copy existing service manifest to new service package
-Copy-Item $CurrentServiceManifestPath $NewSerPkg
+	#
+	# Create a new (config only) application package to deploy to the Service Fabric cluster
+	#
 
-# Copy updated config package to new service package
-$CurrentConfig = [io.Path]::Combine($TraefikPackage, "Config")
-Copy-Item $CurrentConfig -Recurse -Container -Destination $NewSerPkg
+	# Create new application package
+	$NewAppPkgName = "ConfigPkg" + $NewAppTypeVer
+	$NewAppPkg = [io.Path]::Combine($ConfigPackages, $NewAppPkgName)
+	mkdir $NewAppPkg
 
-# Copy existing application manifest to new application package
-Copy-Item $CurrentApplicationManifestPath $NewAppPkg
+	# Create new service package
+	$NewSerPkg = [io.Path]::Combine($NewAppPkg, "TraefikPkg")
+	mkdir $NewSerPkg
 
-#
-# Deploy new application package to Service Fabric cluster
-#
+	# Copy existing service manifest to new service package
+	Copy-Item $CurrentServiceManifestPath $NewSerPkg
 
-$TraefikApplicationName = "fabric:/Traefik"
-Connect-ServiceFabricCluster -ConnectionEndpoint $ServiceFabricConnectionEndpoint
-Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $NewAppPkg -ImageStoreConnectionString $ServiceFabricImageStore -ApplicationPackagePathInImageStore $NewAppPkgName
-Register-ServiceFabricApplicationType -ApplicationPathInImageStore $NewAppPkgName
-Start-ServiceFabricApplicationUpgrade -ApplicationName $TraefikApplicationName -ApplicationTypeVersion $NewAppTypeVer -HealthCheckStableDurationSec 60 -UpgradeDomainTimeoutSec 1200 -UpgradeTimeout 3000 -FailureAction Rollback -Monitored
+	# Copy updated config package to new service package
+	$CurrentConfig = [io.Path]::Combine($TraefikPackage, "Config")
+	Copy-Item $CurrentConfig -Recurse -Container -Destination $NewSerPkg
+
+	# Copy existing application manifest to new application package
+	Copy-Item $CurrentApplicationManifestPath $NewAppPkg
+
+	#
+	# Deploy new application package to Service Fabric cluster
+	#
+
+	$TraefikApplicationName = "fabric:/Traefik"
+	Connect-ServiceFabricCluster -ConnectionEndpoint $ServiceFabricConnectionEndpoint
+	Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $NewAppPkg -ImageStoreConnectionString $ServiceFabricImageStore -ApplicationPackagePathInImageStore $NewAppPkgName
+	Register-ServiceFabricApplicationType -ApplicationPathInImageStore $NewAppPkgName
+	Start-ServiceFabricApplicationUpgrade -ApplicationName $TraefikApplicationName -ApplicationTypeVersion $NewAppTypeVer -HealthCheckStableDurationSec 60 -UpgradeDomainTimeoutSec 1200 -UpgradeTimeout 3000 -FailureAction Rollback -Monitored
+}
+Catch
+{
+	# Revert to original state by overwritting modified files with original backups
+	Copy-Item $CurrentServiceManifestBackupPath $CurrentServiceManifestPath
+	Copy-Item $CurrentApplicationManifestBackupPath $CurrentApplicationManifestPath
+}
+Finally
+{
+	# Remove backup files
+	Remove-Item -Force $CurrentServiceManifestBackupPath
+	Remove-Item -Force $CurrentApplicationManifestBackupPath
+}
