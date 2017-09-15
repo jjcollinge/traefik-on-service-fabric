fec1f721ab7e850912d5c458f1669e1e067ace1d
lawre <lawre@DESKTOP-N48T61D>
2017-09-15 13:01:17 +0100

    Create script to pull down traefik binary

---
+++ Traefik/Scripts/Get-TraefikBinary.ps1
@@ -0,0 +1,20 @@
+﻿#
+# Simple script to pull down the Traefik Binary for deployment. 
+# Overwride URL to upgrade versions to new Binary
+# 
+
+param([string]$url="https://github.com/jjcollinge/traefik-on-service-fabric/releases/download/v0.01/traefik.exe")
+
+Write-Host "Downloading Traefik Binary from $url" -foregroundcolor Green
+Write-Host "to use a specific binary use -url arg" -foregroundcolor Green
+
+$outfile = $PSScriptRoot+"/../ApplicationPackageRoot/TraefikPkg/Code/Traefik.exe"
+
+# Invoke-WebRequest -Uri $url -OutFile $outfile -UseBasicParsing
+
+Write-Host "Download complete" -foregroundcolor Green
+
+Write-Host "Traefik version downloaded:" -foregroundcolor Green
+
+& $outfile version
+
