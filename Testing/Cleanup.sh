a2fbd32ad6d0752b838f047ee0c4c188e22b71cb
Lawrence <info@grippers.co.uk>
2017-11-14 12:49:40 +0000

    Continued work on Load testing #11

---
+++ Testing/Cleanup.sh
@@ -0,0 +1,7 @@
+sfctl application list --query items[].id -o tsv | grep node | xargs -n 1 -P 12 sfctl application delete --application-id
+sfctl application unprovision --application-type-name NodeAppType --application-type-version 1.0.0
+sfctl store delete --content-path loadtest
+
+sfctl application delete --application-id traefik
+sfctl application unprovision --application-type-name TraefikType --application-type-version 1.0.0
+sfctl store delete --content-path ApplicationPackageRoot
--- Testing/Scenario2/Setup.sh
+++ Testing/Scenario2/Setup.sh
@@ -3,11 +3,14 @@ sfctl application upload --path ./Traefik/ApplicationPackageRoot/
 sfctl application provision --application-type-build-path ApplicationPackageRoot
 sfctl application create --app-type TraefikType --app-version 1.0.0 --app-name fabric:/traefik
 
-
+#Spin up some node apps
 sfctl application upload --path ./Demos/apps/loadtest
 sfctl application provision --application-type-build-path loadtest
-for i in {348..500}
+for i in {100..120}
 do
    ( echo "Deploying instance $i"
-   sfctl application create --app-type NodeAppType --app-version 1.0.0 --parameters "{\"PORT\":\"25$i\"}" --app-name fabric:/node25$i ) &
+   sfctl application create --app-type NodeAppType --app-version 1.0.0 --parameters "{\"PORT\":\"25$i\", \"RESPONSE\":\"25$i\"}" --app-name fabric:/node25$i ) &
 done
+
+
+#Run load test
