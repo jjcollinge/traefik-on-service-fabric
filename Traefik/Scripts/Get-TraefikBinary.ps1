722e4c8cdeea9265060b53397a8522c9a6b443bd
lawre <lawre@DESKTOP-N48T61D>
2017-11-30 14:06:30 +0100

    Added watchdog service to report information to appinsights on Traefiks health

--- Traefik/Scripts/Get-TraefikBinary.ps1
+++ Traefik/Scripts/Get-TraefikBinary.ps1
@@ -3,14 +3,19 @@
 # Overwride URL to upgrade versions to new Binary
 # 
 
-param([string]$url="https://github.com/jjcollinge/traefik-on-service-fabric/releases/download/v0.0.5/traefik.exe")
+param(
+	[string]$url="https://github.com/containous/traefik/releases/download/v1.5.0-rc1/traefik_windows-amd64.exe",
+	[string]$urlWatchdog="https://github.com/lawrencegripper/traefik-appinsights-watchdog/releases/download/v0.0.1/traefik-appinsights-watchdog.exe"
+)
 
 Write-Host "Downloading Traefik Binary from $url" -foregroundcolor Green
 Write-Host "to use a specific binary use -url arg" -foregroundcolor Green
 
 $outfile = $PSScriptRoot+"/../ApplicationPackageRoot/TraefikPkg/Code/traefik.exe"
+$outfileWatchdog = $PSScriptRoot+"/../ApplicationPackageRoot/Watchdog/Code/traefik-appinsights-watchdog.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $outfile -UseBasicParsing
+Invoke-WebRequest -Uri $urlWatchdog -OutFile $outfileWatchdog -UseBasicParsing
 
 Write-Host "Download complete" -foregroundcolor Green
 
